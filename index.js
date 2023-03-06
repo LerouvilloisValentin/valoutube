ee8d9e574c3c93f346b70a8d98dd56c86c2ea55b
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2023-03-06 23:20:23 +0100

    commit for deploy

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/users.js"
-import videoRoutes from "./routes/videos.js"
-import commentRoutes from "./routes/comments.js"
-import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-import path from "path"
-import  { fileURLToPath }  from 'url';
-import swaggerJSdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-// const mongoose = require('mongoose')
-
-const app = express();
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = path.dirname(__filename);
+
+
+const app = express();
+dotenv.config()
 
 const connect = () => {
     mongoose.connect(process.env.MONGO_URL,
@@ -26,61 +25,31 @@ const connect = () => {
             throw err;
         });
 };
-console.log(connect)
 
-// const options = {
-
-//     definition: {
-//         openapi: '3.0.0',
-//         info: {
-//             title: 'valoutube',
-//             version: '1.0.0',
-//             description: 'An API made with express and documented with Swagger. For now, all roads can be only tested with insomnia, postman...',
-//             license: {
-//                 name: 'MIT',
-//             }
-//         },
-//         servers: [
-//             {
-//                 url: 'http://localhost:3001',
-//                 description: 'Main production server'
-//             },
-//         ],
-//     },
-
-//     apis: ['./app/routers/*.js'],
-
-// };
-
-// const specs = swaggerJSdoc(options);
-
-// app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.static('client/build'))
+app.use(cookieParser());
+app.use(express.json());
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/videos", videoRoutes)
 app.use("/api/comments", commentRoutes)
 
-app.use((err, req,res, next)=>{
+app.use((err, req, res, next) => {
     const status = err.status || 500;
-    const message = err.message || "Something went wrong";
+    const message = err.message || "Something went wrong!";
     return res.status(status).json({
         success: false,
         status,
         message,
-    }) 
-
+    })
 })
 
-app.get('/*', (_, res) =>{
-    res.sendFile (path.join(__dirname, './client/build','index.html'))
-})
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.listen(process.env.PORT || 3001, ()=>{
-    connect()
-    console.log("connected!");
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
+app.listen(process.env.PORT || 5000, () => {
+    connect()
+    console.log("Connected to server!");
+});
